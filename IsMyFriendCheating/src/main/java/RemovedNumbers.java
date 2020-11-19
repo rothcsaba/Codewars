58c3f054a6163c85f7dd08825091504c1b122267
Róth Csaba <csaba231093@gmail.com>
2020-11-19 22:16:46 +0100

    Is my friend cheating?

---
+++ IsMyFriendCheating/src/main/java/RemovedNumbers.java
@@ -0,0 +1,29 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemovedNumbers {
+
+    public static List<long[]> removNb(long n) {
+        List<long[]> souliton = new ArrayList<>();
+        if (n == 1) {
+            return souliton;
+        }
+        long sum = (n * (n + 1)) / 2;
+        for (long i = 1; i <= n; i++) {
+            double j = (sum - i) / (i + 1.0);
+            if (j <= n && j == (long) j) {
+                souliton.add(new long[]{i, (long) j});
+            }
+        }
+        return souliton;
+    }
+
+    public static void main(String[] args) {
+        List<long[]> longs = removNb(26);
+        for (long[] array: longs) {
+            System.out.println(array[0] + " " + array[1]);
+        }
+    }
+}
